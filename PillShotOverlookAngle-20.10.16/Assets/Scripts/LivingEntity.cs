71b8819866743cadadd440d78456af870f969546
DESKTOP-QESSOSD\Emma <emma.roberts087@gmail.com>
2020-10-25 23:14:17 +0800

    E05，敌人不断以一波生成的形式产生的系统

--- PillShotOverlookAngle-20.10.16/Assets/Scripts/LivingEntity.cs
+++ PillShotOverlookAngle-20.10.16/Assets/Scripts/LivingEntity.cs
@@ -9,6 +9,7 @@ public class LivingEntity : MonoBehaviour, IDamageable
     public float startingHealth;
     protected float health; //protected 为了不出现在引擎，但是能让派生类访问得到
     protected bool isDead;
+    public event System.Action OnDeath; //这是一个事件，关键字 event
 
     protected virtual void Start() //virtual 指父类中的虚方法
     {
@@ -27,6 +28,12 @@ public class LivingEntity : MonoBehaviour, IDamageable
     protected void Die()
     {
         isDead = true;
+        if (OnDeath != null)
+        {
+            OnDeath();//触发事件的意思
+        }
+
         GameObject.Destroy(gameObject);
     }
 }
+
---
+++ PillShotOverlookAngle-20.10.16/Assets/Scripts/Spawner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] Waves;
+    public Enemy enemy;
+    private Wave currentWave; //当前进行的波
+    private int currentWaveNumber; //当前波的数字
+    private int enemiesRemainingToSpawn; //生成点剩余的敌人数量
+    private int enemiesRemainingAlive;
+    private float nextSpawnTime; //下一次生成时间
+
+    private void Start()
+    {
+        NextWave();
+    }
+
+    [Serializable] //可序列化，为了数组在引擎的 Inspector 窗口分别显示
+    public class Wave //类里面写类，可能是一个包含类
+        //一波攻击类中，注明了，一波包含的敌人数量，数量和数量间产生的间隔时间
+    {
+        public int enymyCount;
+        public float timeBetweenSpawns;
+    }
+
+    void NextWave()
+    {
+        currentWaveNumber++;
+        if (currentWaveNumber - 1 < Waves.Length)//这个判断是为了防止数组索引检索报错
+        {
+            currentWave = Waves[currentWaveNumber - 1];
+            enemiesRemainingToSpawn = currentWave.enymyCount;
+            enemiesRemainingAlive = enemiesRemainingToSpawn;
+        }
+    }
+
+    void OnEnemyDeath()
+    {
+        enemiesRemainingAlive--;
+        if (enemiesRemainingAlive == 0)
+        {
+            NextWave();
+        }
+    }
+
+    private void Update()
+    {
+        if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
+        {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+            spawnedEnemy.OnDeath += OnEnemyDeath;
+        }
+    }
+}
