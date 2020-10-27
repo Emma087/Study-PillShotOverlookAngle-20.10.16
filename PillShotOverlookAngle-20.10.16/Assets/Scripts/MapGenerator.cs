8b52c879208d8c7bbc4ab9cc85f3142165cbdbbe
DESKTOP-QESSOSD\Emma <emma.roberts087@gmail.com>
2020-10-27 19:13:04 +0800

    E08.基础地图单块的制作，还有地图块的数值调整代码添加

---
+++ PillShotOverlookAngle-20.10.16/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+    [Range(0, 1)] public float outLinePercent; //创建一个浮动的线条在地板上面
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        string holderName = "Generated Map";
+        if (transform.Find(holderName))//教程写的是 FindChild 这里不知道如何修改
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePositon = new Vector3(-mapSize.x / 2 + .5f + x, 0, -mapSize.y / 2 + .5f + y);
+                //两个 for循环嵌套，只不过是，把正方形的地板一行一个的画出来
+                Transform newTile =
+                    Instantiate(tilePrefab, tilePositon, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                //Quaternion.Euler 围绕 Z轴旋转的意思
+                newTile.localScale = Vector3.one * (1 - outLinePercent); //Vector3.one 就是简写 Vector3(1, 1, 1)。
+                newTile.parent = mapHolder;
+            }
+        }
+        
+    }
+}
